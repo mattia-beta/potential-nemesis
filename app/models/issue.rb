4611d7bda175fe0d47a4fafc84330aa59411e02e
mattia benedetti <mattiabeta@gmail.com>
2013-06-01 12:46:35 +0200

    relation between user and issues

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: issues
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  description :text
+#  priority    :integer
+#  image       :string(255)
+#  location    :string(255)
+#  genre       :string(255)
+#  user_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Issue < ActiveRecord::Base
   belongs_to :user
   attr_accessible :description, :genre, :image, :location, :name, :priority
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,28 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20130601103629) do
+
+  create_table "issues", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "priority"
+    t.string   "image"
+    t.string   "location"
+    t.string   "genre"
+    t.integer  "user_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  add_index "issues", ["user_id"], :name => "index_issues_on_user_id"
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "password"
+    t.string   "email"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
