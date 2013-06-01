9539ee28f7f75da8c116d4e4d266ed61078c86fb
mattia benedetti <mattiabeta@gmail.com>
2013-06-01 13:50:44 +0200

    misssing files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PotentialNemesis::Application.config.secret_token = 'd35c2e5df438572fda2fe169c71a342882f941eaf02d327f7184cd5455980cc8aae47f78f15cd2c5b3a215cbbe42212908aaa30e146f5f3e2840e06d95db53a5'
