450f822181e03c2d62100adbe86ec22dca4385f3
rexos <alex.pellegrini@live.com>
2013-06-02 11:45:44 +0200

    fixed role error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,11 +8,11 @@ class SessionsController < ApplicationController
         if @user and @user.authenticate(params[:session][:password])
             session[:user_id] = @user.id
             cookies.permanent[:token] = @user.token
-            if current_user.role.include? "user"
+            if current_user and current_user.role.include? "user"
                 redirect_to user_path(@user)
-            elsif current_user.role.include? "global"
+            elsif current_user and current_user.role.include? "global"
                 redirect_to controller: :issues, action: :index
-            elsif current_user.role.include? "local"
+            elsif current_user and current_user.role.include? "local"
               redirect_to controller: :issues, action: :local
             end
         else
