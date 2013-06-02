8497735830bcf3a91e8d1d208f67669010f199d6
rexos <alex.pellegrini@live.com>
2013-06-02 11:07:16 +0200

    updated authorizations

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,9 +8,11 @@ class Ability
       can :manage, Issue
       can :manage, User
       cannot :index, Issue
+      cannot :local, :index
       when "local"
       can :manage, Issue
       can :manage, User
+      cannot :show, User
       when "global"
       can :manage, :all
     end
